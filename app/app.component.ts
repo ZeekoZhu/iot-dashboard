913f9794397e9f908361853cbac72c939c5bbe1f
Zeeko Zhu <vaezt@outlook.com>
2016-11-18 01:58:16 +0800

    页面导航逻辑完成一部分，添加了Admin-LTE.d.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Renderer } from '@angular/core';
+
+
+const tabs = [
+    { path: '/dashboard', title: '首页', full: false },
+    { path: '/alarm', title: '报警管理', full: true }
+];
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent implements AfterViewInit {
+    navTabs: NavTab[] = tabs;
+    private _currentTab: NavTab;
+
+    public get currentTab(): NavTab {
+        return this._currentTab;
+    }
+    @ViewChild('content') contentWrapper: ElementRef;
+
+    /**
+     * 当前标签被改变的时候，需要做一些页面布局上的修改
+     * 
+     * @memberOf AppComponent
+     */
+    public set currentTab(value: NavTab) {
+        if (value.full) {
+            this.renderer
+                .setElementStyle(this.contentWrapper.nativeElement, 'margin-left', '0px');
+        } else {
+            this.renderer
+                .setElementStyle(this.contentWrapper.nativeElement, 'margin-left', '230px');
+        }
+        this._currentTab = value;
+    }
+
+    constructor(private renderer: Renderer) { }
+
+    ngAfterViewInit(): void {
+        $.AdminLTE.layout.fix();
+    }
+
+    onTabSelect(tab: NavTab): void {
+        this.currentTab = tab;
+    }
+}
+
+
+/**
+ * NavTab
+ */
+class NavTab {
+    constructor(
+        public path: string,
+        public title: string,
+        public full: boolean
+    ) { }
+}
